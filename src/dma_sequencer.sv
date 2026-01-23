8c00aaf3e59d3e811c921d655328c9cb0e23658b
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2026-01-23 14:32:38 +0530

    Add dma_sequencer class for UVM sequencing

--- src/dma_sequencer.sv
+++ src/dma_sequencer.sv
@@ -1,1 +1,10 @@
 
+class dma_sequencer extends uvm_sequencer #(dma_seq_item);
+  `uvm_component_utils(dma_sequencer)
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
+
