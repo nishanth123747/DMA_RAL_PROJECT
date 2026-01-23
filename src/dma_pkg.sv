4857beeea371469c0ecde0860ad8ff64dcb06e9c
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2026-01-23 14:34:05 +0530

    Add dma_pkg package with UVM includes

--- src/dma_pkg.sv
+++ src/dma_pkg.sv
@@ -1,1 +1,22 @@
+package dma_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "dma_seq_item.sv"
+  `include "dma_sequencer.sv"
+  `include "dma_driver.sv"
+  `include "dma_monitor.sv"
+  `include "dma_agent.sv"
+
+  `include "dma_reg_model.sv"
+  `include "dma_reg_block.sv"
+  `include "dma_adapter.sv"
+
+  `include "dma_coverage.sv"  
+  `include "dma_env.sv"
+
+  `include "dma_sequence.sv"
+  `include "dma_test.sv"
+
+endpackage : dma_pkg
 
