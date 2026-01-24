fdd6ee1f673710aee94dbf85145465c6c2849376
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2026-01-24 14:19:49 +0530

    Update dma_reg_block.sv

--- src/dma_reg_block.sv
+++ src/dma_reg_block.sv
@@ -70,21 +70,46 @@ class dma_reg_block extends uvm_reg_block;
 //    error_status.set_coverage(UVM_CVR_FIELD_VALS);
 //     conf.set_coverage(UVM_CVR_FIELD_VALS);
     
-intr.add_hdl_path_slice("intr_status", 0, 16);   
-intr.add_hdl_path_slice("intr_mask",  16, 16); 
+     intr.add_hdl_path_slice("intr_status", 0, 16);   
+     intr.add_hdl_path_slice("intr_mask",  16, 16); 
     
-ctrl.add_hdl_path_slice("ctrl_start_dma", 0, 1);
-ctrl.add_hdl_path_slice("ctrl_w_count",   1, 15);
-ctrl.add_hdl_path_slice("ctrl_io_mem",   16, 1);
+    ctrl.add_hdl_path_slice("ctrl_start_dma", 0, 1);
+    ctrl.add_hdl_path_slice("ctrl_w_count",   1, 15);
+    ctrl.add_hdl_path_slice("ctrl_io_mem",   16, 1);
    
-io_addr.add_hdl_path_slice("io_addr", 0, 32);
-mem_addr.add_hdl_path_slice("mem_addr", 0, 32);
-extra_info.add_hdl_path_slice("extra_info", 0, 32);
-status.add_hdl_path_slice("status", 0, 32);
-transfer_count.add_hdl_path_slice("transfer_count", 0, 32);
-descriptor_addr.add_hdl_path_slice("descriptor_addr", 0, 32);
-error_status.add_hdl_path_slice("error_status", 0, 32);
-conf.add_hdl_path_slice("conf", 0, 32);
+    io_addr.add_hdl_path_slice("io_addr", 0, 32);
+    
+    mem_addr.add_hdl_path_slice("mem_addr", 0, 32);
+    
+    extra_info.add_hdl_path_slice("extra_info", 0, 32);
+    
+    descriptor_addr.add_hdl_path_slice("descriptor_addr", 0, 32);
+
+    status.add_hdl_path_slice("status_busy", 0, 1);
+    status.add_hdl_path_slice("status_done",1, 1);
+    status.add_hdl_path_slice("status_error",2, 1);
+    status.add_hdl_path_slice("status_paused",3, 1);
+    status.add_hdl_path_slice("status_current_state",4, 4);
+    status.add_hdl_path_slice("status_fifo_level",8, 8);
+
+    transfer_count.add_hdl_path_slice("transfer_count", 0, 32);
+
+ 
+    error_status.add_hdl_path_slice("error_bus", 0, 1);
+    error_status.add_hdl_path_slice("error_timeout", 1, 1);
+    error_status.add_hdl_path_slice("error_alignment",2, 1);
+    error_status.add_hdl_path_slice("error_overflow", 3, 1);
+    error_status.add_hdl_path_slice("error_underflow", 4, 1);
+    error_status.add_hdl_path_slice("error_code", 8, 8);
+    error_status.add_hdl_path_slice("error_addr_offset", 16, 16);
+    
+    conf.add_hdl_path_slice("config_priority", 0, 2);
+    conf.add_hdl_path_slice("config_auto_restart", 2, 1);
+    conf.add_hdl_path_slice("config_interrupt_enable", 3, 1);
+    conf.add_hdl_path_slice("config_burst_size", 4, 2);
+    conf.add_hdl_path_slice("config_data_width", 6, 2);
+    conf.add_hdl_path_slice("config_descriptor_mode",  8, 1);
+
 
     default_map = create_map(
       "default_map",
@@ -98,7 +123,7 @@ conf.add_hdl_path_slice("conf", 0, 32);
     default_map.add_reg(io_addr,'h08, "RW");
     default_map.add_reg(mem_addr,'h0C, "RW");
     default_map.add_reg(extra_info, 'h10, "RW");
-    default_map.add_reg(status, 'h14, "RO");
+    default_map.add_reg(status, 'h14, "RW");
     default_map.add_reg(transfer_count,'h18, "RW");
     default_map.add_reg(descriptor_addr,'h1C, "RW");
     default_map.add_reg(error_status,'h20, "RW");
